a4adda9f6027a57b85ff4ee72644559e9703a4da
DESKTOP-FLS3KBU\30697 <mixzafeir@gmail.com>
2024-10-27 21:22:23 +0200

    initial sql

--- docker-entrypoint-initdb.d/init.sql
+++ docker-entrypoint-initdb.d/init.sql
@@ -1,8 +1,3 @@
--- Create a new user 'myuser' with password 'mypassword'
 CREATE USER IF NOT EXISTS 'etraveli_user'@'%' IDENTIFIED BY 'password';
-
--- Grant all privileges to 'myuser' on all databases
 GRANT ALL PRIVILEGES ON *.* TO 'etraveli_user'@'%';
-
--- Apply the changes
 FLUSH PRIVILEGES;
